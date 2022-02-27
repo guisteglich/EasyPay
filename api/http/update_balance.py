7c61c027bce0d77a1f2e95cec2d359bf990cad69
Guilherme Steglich <guicrga@gmail.com>
2022-02-27 16:44:27 -0300

    working in update balance

--- api/http/update_balance.py
+++ api/http/update_balance.py
@@ -1,6 +1,7 @@
 from crypt import methods
 from flask import Flask, Response, request, jsonify,  Blueprint 
 from api.service import update_balance as service 
+from api.service import list_users
 import json
 from bson.objectid import ObjectId
 
@@ -12,6 +13,8 @@ def update_balance(id):
     value = request.form["value"]
     newvalues = { "$set": { "balance": value } }
     try:
+        data =  list_users.list_user_by_id(id)
+        print("esses são os dados de saldo: ", data[0])
         dbResponse = service.balance_update(id, newvalues)
         return Response(
             response= json.dumps({"message": "user balance updated"}),

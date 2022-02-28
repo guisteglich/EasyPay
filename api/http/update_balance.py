12b7929929d8205afa173a65912de38f662f189d
Guilherme Steglich <guicrga@gmail.com>
2022-02-28 10:53:06 -0300

    finish the funcion update balance

--- api/http/update_balance.py
+++ api/http/update_balance.py
@@ -1,5 +1,6 @@
 from crypt import methods
-from flask import Flask, Response, request, jsonify,  Blueprint 
+from flask import Flask, Response, request, jsonify,  Blueprint
+from psutil import users 
 from api.service import update_balance as service 
 from api.service import list_users
 import json
@@ -11,11 +12,12 @@ balance_update = Blueprint('balance_update', __name__)
 @balance_update.route("/updateBalance/<id>", methods=["PATCH"])
 def update_balance(id):
     value = request.form["value"]
-    newvalues = { "$set": { "balance": value } }
+  
     try:
         data =  list_users.list_user_by_id(id)
-        print("esses são os dados de saldo: ", data[0])
-        dbResponse = service.balance_update(id, newvalues)
+        user = data[0]
+        balance = user["balance"]
+        dbResponse = service.balance_update(id, value, balance)
         return Response(
             response= json.dumps({"message": "user balance updated"}),
             status=200,
--- api/service/update_balance.py
+++ api/service/update_balance.py
@@ -1,6 +1,8 @@
 from hashlib import new
 from api.extensions.mongo import update_user
 
-def balance_update(id, new_values):
+def balance_update(id, value, balance):
+    balance_value_updated = int(value) + int(balance)
+    new_values = { "$set": { "balance": balance_value_updated } }
     response = update_user(id, new_values)
     return response

366aafbbc128c240571fdae2afb34c7bd1b3741d
Guilherme Steglich <guicrga@gmail.com>
2022-03-02 10:24:40 -0300

    confirm if the response is authorization

--- api/http/transaction.py
+++ api/http/transaction.py
@@ -5,6 +5,7 @@ from sqlalchemy import false, true
 from api.service import create_new_transaction as service
 # from api.service import create_new_transaction
 from api.extensions.mongo import delete_transaction
+from api.extensions import before_finish_transaction as confirm
 
 user_transaction = Blueprint('user_transaction', __name__)
 
@@ -18,25 +19,27 @@ def transaction():
             }       
         dbResponse = service.create_new_transaction(transaction)
         print(dbResponse.inserted_id)
-        transaction_process = service.update_values_of_transaction(transaction)
-        print("resposta processo de transição: ", transaction_process)
-        if transaction_process !=  "not ok":
-            return Response(
-                response= json.dumps({
-                    "message": "transaction done with success", 
-                    "id": f"{dbResponse.inserted_id}"
-                    }),
-                status=200,
-                mimetype="application/json"
-            )        
-        else:
-            dbResponse = delete_transaction(id)
-            pass
-            # Response(
-            #     response= json.dumps({"message": "error making transaction"}),
-            #     status=500,
-            #     mimetype="application/json"
-            # )    
+        r = confirm.confirmation()
+        if r == "Autorizado":
+            transaction_process = service.update_values_of_transaction(transaction)
+            #print("resposta processo de transição: ", transaction_process)
+            if transaction_process !=  "not ok":
+                return Response(
+                    response= json.dumps({
+                        "message": "transaction done with success", 
+                        "id": f"{dbResponse.inserted_id}"
+                        }),
+                    status=200,
+                    mimetype="application/json"
+                )        
+            else:
+                dbResponse = delete_transaction(id)
+                pass
+                # Response(
+                #     response= json.dumps({"message": "error making transaction"}),
+                #     status=500,
+                #     mimetype="application/json"
+                # )    
     except Exception as ex:
         print("excessão: ", ex)
         return Response(
